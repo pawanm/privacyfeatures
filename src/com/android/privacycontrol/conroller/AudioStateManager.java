039dabfce6612719c699a39398c03d1783547a12
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-07-18 18:26:34 +0530

    call state manager code modified

--- src/com/android/privacycontrol/conroller/AudioStateManager.java
+++ src/com/android/privacycontrol/conroller/AudioStateManager.java
@@ -8,6 +8,10 @@ public class AudioStateManager
     public void setPhoneState(Context context, int ringerMode)
     {
         AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        switch (ringerMode)
+        {
+            case -1:audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);break;
+            case 1:audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);break;
+        }
     }
 }
--- src/com/android/privacycontrol/conroller/CallListenerManager.java
+++ src/com/android/privacycontrol/conroller/CallListenerManager.java
@@ -3,29 +3,33 @@ package com.android.privacycontrol.conroller;
 import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import com.android.privacycontrol.entities.DeviceContact;
 import com.android.privacycontrol.utils.Logging;
+
+import java.util.List;
 
 public class CallListenerManager extends PhoneStateListener
 {
     AudioStateManager audioStateManager;
+    ContactManager contactManager;
     private Context mContext;
+    List<DeviceContact> favouriteList;
 
     public CallListenerManager(Context context)
     {
         mContext=context;
+        audioStateManager = new AudioStateManager();
+        contactManager = new ContactManager(mContext);
     }
+
 
     @Override
     public void onCallStateChanged (int state, String incomingNumber)
     {
-        audioStateManager = new AudioStateManager();
         if(TelephonyManager.CALL_STATE_RINGING == state)
         {
             Logging.debug("CallListener: Incoming call from " + incomingNumber);
-            if(incomingNumber.contains("9015980883"))
-            {
-                audioStateManager.setPhoneState(mContext,0);
-            }
+            audioStateManager.setPhoneState(mContext,getNumberStatus(incomingNumber));
         }
         if(TelephonyManager.CALL_STATE_OFFHOOK == state)
         {
@@ -35,5 +39,28 @@ public class CallListenerManager extends PhoneStateListener
         {
             Logging.debug("CallListener: Idle");
         }
+    }
+
+    private int getNumberStatus(String incomingNo)
+    {
+        List<DeviceContact> list = contactManager.getRestrictedContactList();
+        for(DeviceContact contact: list)
+        {
+            if(incomingNo.contains(contact.getContactNumber()))
+            {
+                return -1;
+            }
+        }
+
+        list = contactManager.getFavouriteList();
+        for(DeviceContact contact: list)
+        {
+            if(incomingNo.contains(contact.getContactNumber()))
+            {
+                return 1;
+            }
+        }
+
+        return 0;
     }
 }
