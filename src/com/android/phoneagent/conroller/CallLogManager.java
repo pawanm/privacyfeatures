e7ba89dbac430a4d4b33efd2627ca0732876df3e
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-07-19 20:35:22 +0530

    air planame mode removed

--- src/com/android/phoneagent/conroller/CallLogManager.java
+++ src/com/android/phoneagent/conroller/CallLogManager.java
@@ -1,6 +1,7 @@
 package com.android.phoneagent.conroller;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import com.android.phoneagent.utils.Logging;
 
@@ -26,14 +27,16 @@ public class CallLogManager
 
     public void setAirplaneMode(boolean flag)
     {
-        return;
-        /*Logging.debug("SettingAirplaneMode: " + flag);
+
+    }
+
+    private void changeToAirplaneMode(boolean flag)
+    {
         int airplaneMode = flag?1:0;
         Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
         android.provider.Settings.System.putInt(mContext.getContentResolver(), android.provider.Settings.System.AIRPLANE_MODE_ON, airplaneMode);
         intent.putExtra("state", airplaneMode);
-        mContext.sendBroadcast(new Intent("android.intent.action.AIRPLANE_MODE"));
-        mContext.sendBroadcast(intent);*/
+        mContext.sendBroadcast(intent);
     }
 
 
