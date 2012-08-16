7689fa10828d49d299b8d41784503e6cb6a10a14
Pawan Maheshwari <pawan.mrin@gmail.com>
2012-08-16 17:41:04 +0530

    message listener added

--- src/com/android/phoneagent/controller/MessageListenerManager.java
+++ src/com/android/phoneagent/controller/MessageListenerManager.java
@@ -1,8 +1,10 @@
 package com.android.phoneagent.controller;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import com.android.phoneagent.utils.Logging;
@@ -30,6 +32,32 @@ public class MessageListenerManager extends BroadcastReceiver
             }
 
             Logging.debug(strMessage);
+        }
+    }
+
+    public void setAsRead(Context context, String messageId)
+    {
+        ContentValues values = new ContentValues();
+        values.put("read",true);
+        context.getContentResolver().update(Uri.parse("content://sms/"), values, "_id=" + messageId, null);
+    }
+
+    public void setRead(Context context,String SmsNumber, String smsContent)
+    {
+        try {
+          /*  ContentResolver cr = context.getContentResolver();
+            Uri rowUri = Uri.parse("content://sms");
+            Cursor c = context.getContentResolver().query(smsUri, null,null,null,null);
+
+            ContentValues values = new ContentValues();
+            values.put(c.getColumnName(6),"1");
+            cr.update(rowUri, values, " address='"+recnumber+"'and body='"+smsContent+"'", null) ;
+*/
+
+
+        } catch (Exception e)
+        {
+           // Log.i("exception in setRead:", e.getMessage());
         }
     }
 }
